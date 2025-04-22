a92ec09a2a2f02004b81e049ed32b1415f65c70a
Max Lehmann <maxlehmann1404@gmail.com>
2025-04-22 19:04:02 +0200

    Added: i2c config to config package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,26 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"time"
+)
+
+const OUTPUT_WIDTH = 16
 
 type Flags struct {
 	DevMode bool
+}
+
+type I2CDisplayConfig struct {
+	LCDAddress     uint8
+	LCDBus         int
+	LCDLine1       uint8
+	LCDLine2       uint8
+	LCDBacklight   uint8
+	Enable         uint8
+	ReadWrite      uint8
+	RegisterSelect uint8
+	EnableDelay    time.Duration
 }
 
 func GetParsedFlags() *Flags {
@@ -12,5 +29,19 @@ func GetParsedFlags() *Flags {
 
 	return &Flags{
 		DevMode: *devModeFlag,
+	}
+}
+
+func GetI2CDisplayConfig() I2CDisplayConfig {
+	return I2CDisplayConfig{
+		LCDAddress:     0x27,
+		LCDBus:         1,
+		LCDLine1:       0x80,
+		LCDLine2:       0xC0,
+		LCDBacklight:   0x08,
+		Enable:         0x04,
+		ReadWrite:      0x02,
+		RegisterSelect: 0x01,
+		EnableDelay:    1 * time.Millisecond,
 	}
 }
